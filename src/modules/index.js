058fee68ba686ffa298d8bd84e5b6febc5c46d7c
SakibHasanCse <sakibhasan9641@gmail.com>
2022-04-09 19:15:04 +0600

    Improvement app service

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,20 +1,20 @@
-import  fs from "fs"
+import fs from 'fs'
 
 const appModule = async (app) => {
-  const rootPath = __dirname;
-  const moduleNames = await fs.promises.readdir(rootPath);
+  const rootPath = __dirname
+  const moduleNames = await fs.promises.readdir(rootPath)
   await Promise.all(
     moduleNames.map(async (moduleName) => {
-      const stat = await fs.promises.lstat(`${rootPath}/${moduleName}`);
+      const stat = await fs.promises.lstat(`${rootPath}/${moduleName}`)
       if (stat.isDirectory()) {
-        const module = require(`./${moduleName}`);
+        const module = require(`./${moduleName}`)
         if (module.init) {
-          await module.init(app);
+          await module.init(app)
         }
       }
     })
-  );
-  return app;
-};
+  )
+  return app
+}
 
 export default appModule
