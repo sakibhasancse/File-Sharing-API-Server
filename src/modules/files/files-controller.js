dddbdb612ff1f560b080e5cd2d97cab605051068
SakibHasanCse <sakibhasan9641@gmail.com>
2022-04-10 20:31:42 +0600

    updated eslint rules

--- src/modules/files/files-controller.js
+++ src/modules/files/files-controller.js
@@ -1,115 +1,113 @@
-import { verifyToken } from "../../core/jwtToken";
-import { fileTokens, processFile } from "./files-helper"
-import { googleCloudStorage, saveAnFile, getFile, getFileLists, removeFile } from "./files-service";
-import fs from 'fs';
+import { verifyToken } from '../../core/jwtToken'
+import { fileTokens, processFile } from './files-helper'
+import { googleCloudStorage, saveAnFile, getFile, getFileLists, removeFile } from './files-service'
+import fs from 'fs'
 import { size } from 'loadsh'
 
 export const uploadNewFile = async (req, res) => {
-    try {
-        const {  user = {}, body = {} } = req
-        if(!user.userId){
-            return res.status(400).send({message: '`author` is required.'})
-        }
-        await processFile(req, res);
-
-        const { file = {} } = req
-
-        if (!size(file)) {
-            return res.status(400).send({ message: "Please upload a file!" });
-        }
-        const fileData = {
-            name: body.title ? body.title : file.originalname,
-            size: file.size,
-            author: user.userId,
-            path: file.filename,
-        }
-        const result = await saveAnFile(fileData);
-        if (result) {
-            const tokens = await fileTokens(result)
-            res.send(tokens)
-        }
-        // await googleCloudStorage.fileUploadasync(req, res)
-    } catch (err) {
-        res.status(500).send({
-            message: `Could not upload the file. ${err && err.message}`,
-        });
+  try {
+    const { user = {}, body = {} } = req
+    if (!user.userId) {
+      return res.status(400).send({ message: '`author` is required.' })
     }
-};
+    await processFile(req, res)
+
+    const { file = {} } = req
+
+    if (!size(file)) {
+      return res.status(400).send({ message: 'Please upload a file!' })
+    }
+    const fileData = {
+      name: body.title ? body.title : file.originalname,
+      size: file.size,
+      author: user.userId,
+      path: file.filename
+    }
+    const result = await saveAnFile(fileData)
+    if (result) {
+      const tokens = await fileTokens(result)
+      res.send(tokens)
+    }
+    // await googleCloudStorage.fileUploadasync(req, res)
+  } catch (err) {
+    res.status(500).send({
+      message: `Could not upload the file. ${err && err.message}`
+    })
+  }
+}
 export const getListFiles = async (req, res) => {
-    try {
-        const { user = {}, body = {} } = req
-        if (!user.userId) {
-            return res.status(503).send({ message: "Unauthorize user!" });
-        }
-        const files = await getFileLists({ author: user.userId });
-        res.status(200).send(files)
-        // await googleCloudStorage.fileListasync(req, res)
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({
-            message: "Unable to read list of files!",
-        });
+  try {
+    const { user = {}, body = {} } = req
+    if (!user.userId) {
+      return res.status(503).send({ message: 'Unauthorize user!' })
     }
-};
+    const files = await getFileLists({ author: user.userId })
+    res.status(200).send(files)
+    // await googleCloudStorage.fileListasync(req, res)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({
+      message: 'Unable to read list of files!'
+    })
+  }
+}
 
 export const downloadFile = async (req, res) => {
-    try {
-        const { publicToken } = req.params
-        const tokenResult = await verifyToken({ token: publicToken, type: 'File' })
+  try {
+    const { publicToken } = req.params
+    const tokenResult = await verifyToken({ token: publicToken, type: 'File' })
 
-        if (tokenResult && tokenResult.error || !tokenResult) {
-            return res.status(401).json({
-                success: false,
-                message: tokenResult.error
-            })
-        }
-
-        const file = await getFile({ path: tokenResult.data.path })
-        // Link expired
-        if (!file) {
-            return res.json({ error: 'Link has been expired.' });
-        }
-        const filePath = `${__dirname}/../../../assets/upload/${file.path}`;
-        res.download(filePath);
-
-        // await googleCloudStorage.fileDownloadasync(req, res)
-    } catch (err) {
-        res.status(500).send({
-            message: "Could not download the file. " + err,
-        });
+    if (tokenResult && tokenResult.error || !tokenResult) {
+      return res.status(401).json({
+        success: false,
+        message: tokenResult.error
+      })
     }
-};
+
+    const file = await getFile({ path: tokenResult.data.path })
+    // Link expired
+    if (!file) {
+      return res.json({ error: 'Link has been expired.' })
+    }
+    const filePath = `${__dirname}/../../../assets/upload/${file.path}`
+    res.download(filePath)
+
+    // await googleCloudStorage.fileDownloadasync(req, res)
+  } catch (err) {
+    res.status(500).send({
+      message: 'Could not download the file. ' + err
+    })
+  }
+}
 
 export const deleteFile = async (req, res) => {
-    try {
+  try {
+    const { publicToken } = req.params
+    const tokenResult = await verifyToken({ token: publicToken, type: 'File' })
 
-        const { publicToken } = req.params
-        const tokenResult = await verifyToken({ token: publicToken, type: 'File' })
-
-        if (tokenResult && tokenResult.error || !tokenResult || (tokenResult && tokenResult.data && !tokenResult.data._id)) {
-            return res.status(401).json({
-                success: false,
-                message: tokenResult.error || 'Unauthenticated request'
-            })
-        }
-
-        const file = await removeFile({ path: tokenResult.data.path })
-
-        // Link expired
-        if (!file) {
-            return res.json({ error: 'Link has been expired.' });
-        }
-        await fs.unlinkSync(`${__dirname}/../../../assets/upload/${tokenResult.data.path}`);
-
-        return res.json({
-            success: true,
-            message: 'File successfully removed'
-        })
-        // await googleCloudStorage.fileDeleteasync(req, res)
-
-    } catch (err) {
-        res.status(500).send({
-            message: "Could not download the file. " + err,
-        });
+    if (tokenResult && tokenResult.error || !tokenResult || (tokenResult && tokenResult.data && !tokenResult.data._id)) {
+      return res.status(401).json({
+        success: false,
+        message: tokenResult.error || 'Unauthenticated request'
+      })
     }
-};
+
+    const file = await removeFile({ path: tokenResult.data.path })
+
+    // Link expired
+    if (!file) {
+      return res.json({ error: 'Link has been expired.' })
+    }
+    await fs.unlinkSync(`${__dirname}/../../../assets/upload/${tokenResult.data.path}`)
+
+    return res.json({
+      success: true,
+      message: 'File successfully removed'
+    })
+    // await googleCloudStorage.fileDeleteasync(req, res)
+  } catch (err) {
+    res.status(500).send({
+      message: 'Could not download the file. ' + err
+    })
+  }
+}
--- src/modules/files/files-helper.js
+++ src/modules/files/files-helper.js
@@ -1,30 +1,29 @@
-import util from "util";
-import multer from "multer";
+import util from 'util'
+import multer from 'multer'
 import GoogleCloudStorage from '@google-cloud/storage'
-const fileSize = process.env.MAX_FILE_SIZE || 2
-const maxSize = fileSize * 1024 * 1024;
-const hasCloudStorage = process.env.GOOGLE_CLOUD_PROJECT_ID ? true : false;
-import path from 'path';
+import path from 'path'
 import { JwtToken } from './../../core'
+const fileSize = process.env.MAX_FILE_SIZE || 2
+const maxSize = fileSize * 1024 * 1024
+const hasCloudStorage = !!process.env.GOOGLE_CLOUD_PROJECT_ID
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, '../../../assets/upload/'))
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     const fileExtension = file.originalname.split('.').pop()
-    const fileName = `${uniqueSuffix}.${fileExtension}`;
-    cb(null, fileName);
+    const fileName = `${uniqueSuffix}.${fileExtension}`
+    cb(null, fileName)
   }
 })
 
-let multerConfig = multer({
+const multerConfig = multer({
   storage: hasCloudStorage ? multer.memoryStorage() : storage,
-  limits: { fileSize: maxSize },
-}).single("file");
+  limits: { fileSize: maxSize }
+}).single('file')
 
-export const processFile = util.promisify(multerConfig);
-
+export const processFile = util.promisify(multerConfig)
 
 // export const storage = GoogleCloudStorage({
 //   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
@@ -32,8 +31,8 @@ export const processFile = util.promisify(multerConfig);
 // });
 
 export const fileTokens = async (file) => {
-  const publicToken = await JwtToken({ type: 'File', data: { path: file.path }, expiresIn: `${process.env.FILE_EXPIRE}` });
-  const privateToken = await JwtToken({ type: 'File', data: file, expiresIn: `${process.env.FILE_EXPIRE}` });
+  const publicToken = await JwtToken({ type: 'File', data: { path: file.path }, expiresIn: `${process.env.FILE_EXPIRE}` })
+  const privateToken = await JwtToken({ type: 'File', data: file, expiresIn: `${process.env.FILE_EXPIRE}` })
   const tokens = {
     publicToken,
     privateToken
